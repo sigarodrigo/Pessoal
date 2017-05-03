9b65f0773958c0c53eb0db81666f4d61acb73546
Rodrigo Resende <siga.rodrigo@gmail.com>
2017-05-02 23:39:21 -0300

    Add files via upload

--- Sistema.Lara.Carolina/Sistema.Lara.Carolina/CadCliente.Designer.cs
+++ Sistema.Lara.Carolina/Sistema.Lara.Carolina/CadCliente.Designer.cs
@@ -28,19 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCli1 = new System.Windows.Forms.Button();
+            this.btnCli2 = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblCli1 = new System.Windows.Forms.Label();
+            this.lblCli2 = new System.Windows.Forms.Label();
+            this.comboBoxSexo = new System.Windows.Forms.ComboBox();
+            this.comboBoxEst = new System.Windows.Forms.ComboBox();
+            this.lblCli3 = new System.Windows.Forms.Label();
+            this.lblCli4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
+            // 
+            // btnCli1
+            // 
+            this.btnCli1.Location = new System.Drawing.Point(339, 270);
+            this.btnCli1.Name = "btnCli1";
+            this.btnCli1.Size = new System.Drawing.Size(87, 49);
+            this.btnCli1.TabIndex = 0;
+            this.btnCli1.Text = "Confirmar";
+            this.btnCli1.UseVisualStyleBackColor = true;
+            // 
+            // btnCli2
+            // 
+            this.btnCli2.Location = new System.Drawing.Point(432, 270);
+            this.btnCli2.Name = "btnCli2";
+            this.btnCli2.Size = new System.Drawing.Size(83, 49);
+            this.btnCli2.TabIndex = 1;
+            this.btnCli2.Text = "Cancelar";
+            this.btnCli2.UseVisualStyleBackColor = true;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 68);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(289, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // lblCli1
+            // 
+            this.lblCli1.AutoSize = true;
+            this.lblCli1.Font = new System.Drawing.Font("Lucida Handwriting", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCli1.Location = new System.Drawing.Point(101, 9);
+            this.lblCli1.Name = "lblCli1";
+            this.lblCli1.Size = new System.Drawing.Size(306, 36);
+            this.lblCli1.TabIndex = 3;
+            this.lblCli1.Text = "Cadastrar Cliente";
+            // 
+            // lblCli2
+            // 
+            this.lblCli2.AutoSize = true;
+            this.lblCli2.Location = new System.Drawing.Point(13, 49);
+            this.lblCli2.Name = "lblCli2";
+            this.lblCli2.Size = new System.Drawing.Size(85, 13);
+            this.lblCli2.TabIndex = 4;
+            this.lblCli2.Text = "Nome Completo:";
+            // 
+            // comboBoxSexo
+            // 
+            this.comboBoxSexo.FormattingEnabled = true;
+            this.comboBoxSexo.Location = new System.Drawing.Point(307, 67);
+            this.comboBoxSexo.Name = "comboBoxSexo";
+            this.comboBoxSexo.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxSexo.TabIndex = 5;
+            // 
+            // comboBoxEst
+            // 
+            this.comboBoxEst.FormattingEnabled = true;
+            this.comboBoxEst.Location = new System.Drawing.Point(413, 67);
+            this.comboBoxEst.Name = "comboBoxEst";
+            this.comboBoxEst.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxEst.TabIndex = 6;
+            // 
+            // lblCli3
+            // 
+            this.lblCli3.AutoSize = true;
+            this.lblCli3.Location = new System.Drawing.Point(308, 49);
+            this.lblCli3.Name = "lblCli3";
+            this.lblCli3.Size = new System.Drawing.Size(34, 13);
+            this.lblCli3.TabIndex = 7;
+            this.lblCli3.Text = "Sexo:";
+            // 
+            // lblCli4
+            // 
+            this.lblCli4.AutoSize = true;
+            this.lblCli4.Location = new System.Drawing.Point(413, 48);
+            this.lblCli4.Name = "lblCli4";
+            this.lblCli4.Size = new System.Drawing.Size(50, 13);
+            this.lblCli4.TabIndex = 8;
+            this.lblCli4.Text = "Est. Civil:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Endereço:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 156);
+            this.maskedTextBox1.Mask = "(999) 00000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Telefone 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Telefone 2:";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(132, 156);
+            this.maskedTextBox2.Mask = "(999) 00000-0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox2.TabIndex = 13;
             // 
             // CadCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 331);
+            this.ClientSize = new System.Drawing.Size(527, 331);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblCli4);
+            this.Controls.Add(this.lblCli3);
+            this.Controls.Add(this.comboBoxEst);
+            this.Controls.Add(this.comboBoxSexo);
+            this.Controls.Add(this.lblCli2);
+            this.Controls.Add(this.lblCli1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnCli2);
+            this.Controls.Add(this.btnCli1);
             this.Name = "CadCliente";
             this.Text = "Cadastro de Clientes";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCli1;
+        private System.Windows.Forms.Button btnCli2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblCli1;
+        private System.Windows.Forms.Label lblCli2;
+        private System.Windows.Forms.ComboBox comboBoxSexo;
+        private System.Windows.Forms.ComboBox comboBoxEst;
+        private System.Windows.Forms.Label lblCli3;
+        private System.Windows.Forms.Label lblCli4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
     }
 }
